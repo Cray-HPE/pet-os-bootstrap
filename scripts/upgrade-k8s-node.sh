1d49f241e1690389a1d9247e4c065122d054d6c9
Brad Klein <bklein@cray.com>
2021-03-17 12:54:41 -0600

    More functionality in upgrade scripts

--- scripts/upgrade-k8s-node.sh
+++ scripts/upgrade-k8s-node.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-while getopts n: options
+while getopts n:f: options
 do
   case "${options}" in
           n) rebuild_node=$OPTARG;;
+          f) first_master=$OPTARG;;
   esac
 done
 
-#
-# TODO: get 'first' node and use that below -- assuming ncn-m002 for now
-#
-first_master=ncn-m002
-
 if [[ "$rebuild_node" =~ ^ncn-w ]]; then
   echo "Draining $rebuild_node"
-  ssh $first_master "kubectl drain --ignore-daemonsets --delete-local-data $rebuild_node"
+  pdsh -w $first_master "kubectl drain --ignore-daemonsets --delete-local-data $rebuild_node"
   echo "sleeping 10 second...zzz.."
   sleep 10
   echo "Removing $rebuild_node from cluster"
-  ssh $first_master "kubectl delete node $rebuild_node"
+  pdsh -w $first_master "kubectl delete node $rebuild_node"
 
 elif [[ "$rebuild_node" =~ ^ncn-m ]]; then
 
@@ -26,17 +22,17 @@ elif [[ "$rebuild_node" =~ ^ncn-m ]]; then
     echo "this is first master -- do special things" 
   else
     echo "Stopping etcd on $rebuild_node"
-    ssh $rebuild_node "systemctl stop etcd.service"
+    pdsh -w $rebuild_node "systemctl stop etcd.service"
     echo "Removing $rebuild_node from etcd cluster"
-    ssh $first_master "etcdctl --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/ca.crt  --key=/etc/kubernetes/pki/etcd/ca.key --endpoints=localhost:2379 member remove $(etcdctl --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/ca.crt  --key=/etc/kubernetes/pki/etcd/ca.key --endpoints=localhost:2379 member list | grep $rebuild_node | cut -d , -f1)"
+    pdsh -w  $first_master "etcdctl --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/ca.crt  --key=/etc/kubernetes/pki/etcd/ca.key --endpoints=localhost:2379 member remove \$(etcdctl --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/ca.crt  --key=/etc/kubernetes/pki/etcd/ca.key --endpoints=localhost:2379 member list | grep $rebuild_node | cut -d , -f1)"
     echo "Removing $rebuild_node from cluster"
-    ssh $first_master "kubectl delete node $rebuild_node"
+    pdsh -w $first_master "kubectl delete node $rebuild_node"
   fi
 fi
 
-if [[ "$rebuild_node" =~ ^ncn-m ]]; then
-  exit 0
-fi
+#if [[ "$rebuild_node" =~ ^ncn-m ]]; then
+#  exit 0
+#fi
 
 echo "Deleting server $rebuild_node"
 openstack server delete $rebuild_node
@@ -50,8 +46,6 @@ sed -i -e '/DELETE_BELOW/q0' /etc/hosts
 
 echo "Creating boot volumes for node ${rebuild_node}"
 openstack volume create --size 45 --type RBD --snapshot k8s-1.19-gold  --bootable $rebuild_node
-
-for node in $(openstack server list -f json| jq -r .[].Name); do  echo "$(openstack server show -f json $node|jq -r .addresses|cut -d = -f2) $node" >> hosts; done
 
 counter=1
 until [ $counter -eq 0 ]
@@ -74,8 +68,9 @@ echo "sleeping 20 second...zzz.."
 sleep 20
 
 echo "Creating vms for K8S"
-openstack server create  --flavor highcpu.4  --key-name craystack --user-data init.sh  --volume $rebuild_node --network Cray_Network --file /etc/hosts=hosts $rebuild_node
+openstack server create  --flavor highcpu.4  --key-name craystack --user-data init.sh  --volume $rebuild_node --network Cray_Network $rebuild_node
 
+new_ip=""
 cp /var/www/ephemeral/configs/data.orig /var/www/ephemeral/configs/data.json
 for node in $(openstack server list -f json| jq -r .[].Name|egrep -i 'ncn-')
 do
@@ -91,8 +86,20 @@ do
  echo "Node $node has ip: $ip and mac: $mac"
  echo "$ip $node $node.nmn" >> /etc/hosts
  sed -i -e "s/mac-$node/$mac/" /var/www/ephemeral/configs/data.json
+
+ if [[ "$rebuild_node" == "$node" ]]; then
+   new_ip=$ip
+ fi
+
 done
 
 echo "Restarting dnsmasq and basecamp"
 systemctl restart dnsmasq.service
 podman restart basecamp
+
+if [[ "$rebuild_node" =~ ^ncn-m ]]; then
+  echo "Adding $rebuild_node back into etcd cluster with new ip ($new_ip)"
+  pdsh -w $first_master "etcdctl --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/ca.crt --key=/etc/kubernetes/pki/etcd/ca.key --endpoints=localhost:2379 member add ncn-m003 --peer-urls=https://$new_ip:2380"
+  echo "Changing initial cluster state for etcd on $rebuild_node to 'existing' before it tries to join"
+  pdsh -w $rebuild_node "sed -i 's/new/existing/' /srv/cray/resources/common/etcd/etcd.service"
+fi
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
-first_master=ncn-m002
-workers=$(pdsh -w $first_master "kubectl get nodes | grep ncn-w" | awk '{print $2}')
+#
+# FUNCTIONS
+#
+function get_first_master() {
+  first_master=$(cat /var/www/ephemeral/configs/data.json | jq -r '.Global."meta-data"."first-master-hostname"')
+  echo $first_master
+}
 
-for worker in $workers; do
-  ./upgrade-k8s-node.sh -n $worker
-  state=$(pdsh -w $first_master "kubectl get nodes | grep $worker" 2>&1 | awk '{print $3}')
+function wait_for_node() {
+  local first_master=$1
+  local node=$2
+  state=$(pdsh -w $first_master "kubectl get nodes | grep $node" 2>&1 | awk '{print $3}')
   cnt=0
   until [ "$state" == "Ready" ]; do
-     echo "Waiting for $worker to become 'Ready'"
+     echo "Waiting for $node to become 'Ready'"
      sleep 5
      cnt=+1
      if [ "$cnt" -ge 60 ]; then
-       echo "ERROR: $worker isn't ready after waiting 5 minutes -- aborting!"
+       echo "ERROR: $node isn't ready after waiting 5 minutes -- aborting!"
        exit 1
      fi
-     state=$(pdsh -w $first_master "kubectl get nodes | grep $worker" 2>&1 | awk '{print $3}')
+     state=$(pdsh -w $first_master "kubectl get nodes | grep $node" 2>&1 | awk '{print $3}')
   done
-  echo "SUCCESS: $worker is now in a ready state"
+  echo "SUCCESS: $node is now in a ready state"
+}
+
+#
+# EXECUTION
+#
+first_master=$(get_first_master)
+masters=$(pdsh -w $first_master "kubectl get nodes | grep ncn-m" | awk '{print $2}')
+for master in $masters; do
+  if [ "$master" == "$first_master" ]; then
+     echo "BRAD: skipping upgrade of $first_master for now"
+     continue
+  fi
+  ./upgrade-k8s-node.sh -n $master -f $first_master
+  wait_for_node $first_master $master
+done
+
+workers=$(pdsh -w $first_master "kubectl get nodes | grep ncn-w" | awk '{print $2}')
+for worker in $workers; do
+  ./upgrade-k8s-node.sh -n $worker -f $first_master
+  wait_for_node $first_master $worker
 done
